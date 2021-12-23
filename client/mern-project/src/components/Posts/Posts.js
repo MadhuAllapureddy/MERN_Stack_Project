4c5485a02c3859d4da65bb34de31fa99bb939965
MadhuAllapureddy <93430740+MadhuAllapureddy@users.noreply.github.com>
2021-12-23 07:48:00 +0530

    latest code

---
+++ client/mern-project/src/components/Posts/Posts.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Post from "./Post/Post";
+import useStyles from './styles';
+import { useSelector } from "react-redux";
+import { Grid, CircularProgress } from "@material-ui/core";
+
+
+const Posts =() => {
+    const posts = useSelector((state) => state.posts);
+    const classes = useStyles();
+
+    console.log(posts);
+    return (
+        !posts.length ? <CircularProgress /> :
+    )
+}
+
+
+export default Posts;
